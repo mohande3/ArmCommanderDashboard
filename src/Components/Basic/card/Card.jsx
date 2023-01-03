1188b9f3406d74385625997897079c307c69b5ec
mohamd taghi khademi <mohande3khademi@hotmail.com>
2023-01-02 19:52:51 -0800

    Add Card Component

--- src/Components/Basic/card/Card.jsx
+++ src/Components/Basic/card/Card.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
-function Card({ className = "", id = "" }) {
-  return <div>Card</div>;
+function Card({ className = "", id = "", title = "", titleElement,children }) {
+  return (
+    <>
+      <div id={id} class={"card h-100 " + className}>
+        <div class="card-body">
+          {titleElement === undefined ? (
+            <h5 class="card-title">{title}</h5>
+          ) : (
+            <>{titleElement}</>
+          )}
+          { children}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Card;
